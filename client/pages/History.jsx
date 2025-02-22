b2e0746def839b6c67d9d9e1d0e76333db554d10
Lokeshvenkateshan <lokeshvenkatesan44@gmail.com>
2025-02-22 22:14:51 +0530

    history style

--- client/pages/History.jsx
+++ client/pages/History.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
-
+import "../pagestyling/History.css"
 const History = () => {
   const user = useSelector((state) => state.userInfo.user);
   const usertoken = user?.token;
@@ -18,8 +18,8 @@ const History = () => {
 
   return (
     <>
-      <h2>History</h2>
-      <ul>
+      
+      {/* <ul>
         {history.map((his, index) => (
           <li key={index}>
             <h2>{new Date(his.createdAt).toLocaleString()}</h2>
@@ -32,7 +32,39 @@ const History = () => {
             }
           </li> 
         ))}
-      </ul>
+      </ul> */}
+
+
+
+    <div className='history-outer'>
+      <center><h2 className='history-heading'><u>History</u></h2></center>
+      <div className="table-container">
+        <table className="history-table">
+          <thead>
+            <tr>
+              <th ><center>Date & Time</center></th>
+              <th className="short-content"><center>Short Content</center></th>
+              <th><center>Actions</center></th>
+            </tr>
+          </thead>
+          <tbody>
+            {history.map((his, index) => (
+              <tr key={index}>
+                <td>{new Date(his.createdAt).toLocaleString()}</td>
+                <td className="short-content">{his.shortContent}</td>
+                <td>
+                  {his._id && (
+                    <a href={`http://localhost:5001/download-pdf/${his._id}`}>
+                      <button className="view-pdf">View PDF</button>
+                    </a>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
     </>
   );
 };
