cac1d837484c785e1223712ba7a8ce528e8d51c5
gajaendrran <gaja0902ra@gmail.com>
2025-03-15 09:23:01 +0530

    Changed the cors

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ const connectDB = require('./db/connect');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    { origin: ['http://localhost:5173', 'https://smarfert.vercel.app/'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true}));
 
 
 connectDB();
