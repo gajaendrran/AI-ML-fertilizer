8840186052c64db1f15175a3d1b14754a5fd65ae
gajaendrran <gaja0902ra@gmail.com>
2025-03-15 10:47:08 +0530

    Changed the crop drop down

--- client/data/CropTypes.jsx
+++ client/data/CropTypes.jsx
@@ -1,6 +1,5 @@
 const cropTypes = ['Barley', 'Coffee', 'Cotton', 'Ground Nuts', 'Kidneybeans', 'Maize', 'Millets', 
-    'Oil Seeds', 'Orange', 'Paddy', 'Pomegranate', 'Pulses', 'Rice', 'Sugarcane', 
-    'Tobacco', 'Watermelon', 'Wheat']
+    'Oil Seeds', 'Orange', 'Paddy', 'Pomegranate', 'Pulses', 'Rice', 'Sugarcane', 'Watermelon', 'Wheat']
    ;
 
 export default cropTypes;
