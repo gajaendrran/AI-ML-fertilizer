0639ea9bbe0b176e40345385b5e44703d9372794
gajaendrran <gaja0902ra@gmail.com>
2025-04-16 15:53:22 +0530

    Ferticompo

---
+++ server/composition/ferticompo.js
@@ -0,0 +1,19 @@
+// Fertilizer compositions
+const fertilizerCompositions = {
+    "DAP": { N: 18, P: 46, K: 0 },
+    "Urea": { N: 46, P: 0, K: 0 },
+    "Superphosphate": { N: 0, P: 16, K: 0 },
+    "TSP": { N: 0, P: 46, K: 0 },
+    "Potassium chloride": { N: 0, P: 0, K: 60 },
+    "Potassium sulfate": { N: 0, P: 0, K: 50 },
+    "10-10-10": { N: 10, P: 10, K: 10 },
+    "10-26-26": { N: 10, P: 26, K: 26 },
+    "14-14-14": { N: 14, P: 14, K: 14 },
+    "14-35-14": { N: 14, P: 35, K: 14 },
+    "15-15-15": { N: 15, P: 15, K: 15 },
+    "17-17-17": { N: 17, P: 17, K: 17 },
+    "20-20": { N: 20, P: 20, K: 0 },
+    "28-28": { N: 28, P: 28, K: 0 }
+};
+
+modules.exports = fertilizerCompositions;
